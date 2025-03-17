23e26baf3acca5df06fb65b957e773c9e973cff0
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-03-17 23:05:45 +0500

    make logic with array along with string

--- backtracking.java
+++ backtracking.java
@@ -1,21 +1,44 @@
-
-public class backtracking{
-    public static void permutation(String str, String perm, int index){
-        if (str.length()==0) {
+public class backtracking {
+    // String Permutation (Same as before)
+    public static void permutation(String str, String perm) {
+        if (str.length() == 0) {
             System.out.println(perm);
             return;
-            
-        }
-        for(int i=0;i<str.length();i++){
-            char curr = str.charAt(i);
-            String newStr = str.substring(0, i) + str.substring(i+1);
-            permutation(newStr, perm + curr, index + 1);
         }
 
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            permutation(newStr, perm + curr);
+        }
     }
+
+    // Number Permutation Using Boolean Array
+    public static void numPermutation(int arr[], boolean used[], String perm) {
+        if (perm.length() == arr.length * 2 - 1) { // Because numbers are separated by spaces
+            System.out.println(perm);
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                numPermutation(arr, used, perm + arr[i] + " "); // Add number with space
+                used[i] = false; // Backtrack
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String str ="ABC";
-        permutation(str, "", 0);
-    
+        // String Permutations
+        String str = "ABC";
+        System.out.println("String Permutations:");
+        permutation(str, "");
+
+        // Number Permutations
+        int arr[] = {1, 2, 3};
+        boolean used[] = new boolean[arr.length]; // Track used elements
+        System.out.println("\nNumber Permutations:");
+        numPermutation(arr, used, "");
     }
 }
